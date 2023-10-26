7b3a1c351533c56d6f97eb742bdbcc24c4f5594d
Vikram Singh <113547823+Viku69@users.noreply.github.com>
2023-10-26 19:33:54 +0530

    Initial Commit

---
+++ Uni Restro/Uni_RestroApp.swift
@@ -0,0 +1,17 @@
+//
+//  Uni_RestroApp.swift
+//  Uni Restro
+//
+//  Created by Vikram Singh on 26/10/23.
+//
+
+import SwiftUI
+
+@main
+struct Uni_RestroApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
